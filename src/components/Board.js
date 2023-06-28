85607517711fdad7c9f354e2721dfaff66396e9c
Truc Tran <tracytrle@gmail.com>
2023-06-27 21:23:52 -0500

    working on undo

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,12 +2,36 @@ import React, { useState } from "react";
 import Square from "./Square";
 
 export default function Board({ squares, handleClick }) {
+  const firstRow = squares.slice(0, 3);
+  const secondRow = squares.slice(3, 6);
+  const thirdRow = squares.slice(-3);
+
   return (
     <div className="board">
       <div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
+        <div className="board-row">
+          {firstRow.map((cell, cellIndex) => (
+            <Square index={cellIndex} symbol={cell} handleClick={handleClick} />
+          ))}
+        </div>
+        <div className="board-row">
+          {secondRow.map((cell, cellIndex) => (
+            <Square
+              index={3 + cellIndex}
+              symbol={cell}
+              handleClick={handleClick}
+            />
+          ))}
+        </div>
+        <div className="board-row">
+          {thirdRow.map((cell, cellIndex) => (
+            <Square
+              index={6 + cellIndex}
+              symbol={cell}
+              handleClick={handleClick}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,3 +1,4 @@
+import { set } from "lodash";
 import React, { useState, useEffect } from "react";
 import Board from "./Board";
 
@@ -5,10 +6,25 @@ function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null);
+  const [buttons, setButtons] = useState([]);
+  const [currentMove, setCurrentMove] = useState(null);
+  const [history, setHistory] = useState([]);
+  // const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  // const current = history[history.length - 1];
+  // // const squares = current.squares;
 
   //Declaring a Winner
+
   useEffect(() => {
-    "Your code here";
+    // "Your code here";
+    console.log("test useEffect");
+
+    const result = calculateWinner(squares);
+    if (!result) {
+    }
+    console.log("test useEffect with result: ", result);
+    // not allow to play when the winner is defined
+    setWinner(result);
   }, [squares]);
 
   //function to check if a player has won.
@@ -38,14 +54,74 @@ function Game() {
     return null;
   };
 
+  function jumpTo(move) {
+    setCurrentMove(move);
+    const newHistory = history.slice(0, move);
+    setHistory(newHistory);
+
+    const newButtons = buttons.slice(0, move);
+    setButtons(newButtons);
+  }
+
   //Handle player
   const handleClick = (i) => {
-    "Your code here";
+    // "Your code here";
+    // prevent handleClick from being called when the game has already been won
+    if (winner) return;
+
+    const copiedSquares = squares.slice();
+    copiedSquares[i] = xIsNext ? "X" : "O";
+    setSquares(copiedSquares);
+    setXIsNext(!xIsNext);
+
+    // set HistoryMove
+    // setHistory(history.concat([{ squares: copiedSquares }]));
+    const newHistory = [...history, currentMove + 1];
+    setHistory(newHistory);
+
+    // setHistory(history.slice(0, currentMove + 1).concat([{ squares: newSquares }]));
+
+    const newButtons = buttons.slice();
+    newButtons.push(
+      <button
+        key={newButtons.length + 1}
+        onClick={() => jumpTo(newButtons.length + 1)}
+      >
+        {`Go to move #${newButtons.length + 1}`}
+      </button>
+    );
+    setButtons(newButtons);
   };
 
   //Restart game
   const handlRestart = () => {
-    "Your code here";
+    // "Your code here";
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+    setWinner(null);
+    setButtons([]);
+  };
+
+  // const history = () => {
+  //   return (
+  //     <li>
+  //     </li>
+  //   );
+  // };
+  const undo = () => {
+    if (currentMove === 0) return;
+
+    // if won before undo
+    setWinner(null);
+
+    const newHistory = history.slice(0, history.length - 1);
+    setHistory(newHistory);
+
+    const newButtons = buttons.slice(0, buttons.length - 2);
+    setCurrentMove(currentMove - 1);
+    setButtons(newButtons);
+
+    setXIsNext(xIsNext ? false : true);
   };
 
   return (
@@ -53,9 +129,29 @@ function Game() {
       <h2 className="result">Winner is: {winner ? winner : "N/N"}</h2>
       <div className="game">
         <span className="player">Next player is: {xIsNext ? "X" : "O"}</span>
-        <Board squares={"Your code here"} handleClick={"Your code here"} />
+        <Board squares={squares} handleClick={handleClick} />
+
+        <div class="history">
+          <h4>History</h4>
+          <ul>
+            {buttons.map((button, move) => (
+              <li key={move}>
+                <button
+                  className={move === currentMove ? "selected" : ""}
+                  onClick={() => jumpTo(move)}
+                >
+                  {button.props.children}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <span></span>
+        <div class="undo-btn">
+          <button onClick={undo}>Undo</button>
+        </div>
       </div>
-      <button onClick={"Your code here"} className="restart-btn">
+      <button onClick={handlRestart} className="restart-btn">
         Restart
       </button>
     </div>
